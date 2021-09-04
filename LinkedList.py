f6a605dd3534a9499eb11245ce8ac2f1cc7c0989
Invited User iueghpds <iueghpds@mail.gitlabdemo.cloud>
2021-09-05 01:39:17 +0530

    Comminting latest changes

--- LinkedList.py
+++ LinkedList.py
@@ -23,7 +23,7 @@ class LinkedList:
             while temp is not None:
                 print(temp.value, end = '')
                 if temp != self.tail:
-                    print (",", end = '')
+                    print ("->", end = '')
                 temp=temp.next
         
         print ("}")
@@ -40,24 +40,106 @@ class LinkedList:
             self.tail=new_node
         
         self.length=self.length+1
+
+        return True
     
     #Func to remove last node from the list
     def pop(self):
-        temp=self.head
 
-        if self.head  == self.tail:
+        if self.length == 0:
+            return False
+        elif self.head  == self.tail:
             self.head=None
             self.tail=None
         else:
+            temp=self.head
             while temp.next != self.tail:
                 temp=temp.next
             self.tail=temp
             temp.next=None
 
         self.length=self.length-1
+        return True
 
+    def prepend(self,value):
+        new_node=Node(value)
+
+        if self.head == None and self.tail == None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        
+        self.length=self.length+1
+
+        return True
+
+    def pop_first(self):
+
+        if self.length == 0:
+            return False
+        elif self.head  == self.tail:
+            self.head=None
+            self.tail=None
+        else:
+            first_on_list=self.head
+            self.head=first_on_list.next
+            first_on_list.next=None
+
+        self.length=self.length-1
+
+        return True
+    
+    def get(self,index):
+        if self.length == 0 or index < 0 or index > self.length-1:
+            return None
+
+        temp = self.head
+        for _ in range(index):
+            temp=temp.next
+        
+        return temp
+    
+    def set_value(self,index,value):            
+        temp=self.get(index)
+        if temp:
+            temp.value=value
+            return True
+        return False
+
+    def insert(self,index,value):
+        if index < 0 or index > self.length:
+            return False
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.append(value)
+        else:
+            temp=self.get(index-1)
+            new_node=Node(value)
+            new_node.next=temp.next
+            temp.next=new_node
+            self.length+=1
+            return True
+    
+    def remove(self,index):
+
+        if index < 0 or index > self.length:
+            return False
+        elif index == 0:
+            return self.pop_first()
+        elif index == self.length:
+            return self.pop()
+        else:
+            pre=self.get(index-1)
+            post=self.get(index+1)
+            pre.next.next=None
+            pre.next=post
+            return True
 
 if __name__ == '__main__':
+
     test_linked_list=LinkedList(4)
 
     test_linked_list.append(8)
@@ -70,5 +152,15 @@ if __name__ == '__main__':
     test_linked_list.pop()
     test_linked_list.pop()
     test_linked_list.append(11)
-
+    test_linked_list.prepend(14)
     test_linked_list.printList()
+    test_linked_list.pop_first()
+    test_linked_list.printList()
+    #print(test_linked_list.get(1).value)
+    test_linked_list.set_value(0,20)
+    test_linked_list.printList()
+    test_linked_list.insert(2,40)
+    test_linked_list.printList()
+    test_linked_list.remove(2)
+    test_linked_list.printList()
+    
