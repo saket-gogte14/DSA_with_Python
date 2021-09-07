8039b7ce3b8c15f26c29dac4f46200451abeb69d
Saket Gogte <saket.gogte14@gmail.com>
2021-09-07 11:17:39 +0530

    Refining BST Code

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -3,7 +3,7 @@
 import os
 #Class Node to instantiate a node object
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
@@ -11,39 +11,37 @@ class Node:
 #Class BinarySearchTree to instantiate a node object
 class BinarySearchTree:
     def __init__(self):
-        self.rootptr = None
+        self.root = None
     
-    def insert(self,value):
-        new_node=Node(value)
-        if self.rootptr is None:
-            self.rootptr=new_node
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
             return True
-
-        temp = self.rootptr
-        
-        while (True):
-            
-            if int(value) < int(temp.value):
-                print(self.rootptr)
-                if temp.left is not None:
+        temp = self.root
+        while temp is not None:
+            if new_node.value == temp.value:
+                return False
+            elif new_node.value < temp.value:
+                if temp.left is None:
                     temp.left = new_node
                     return True
                 temp = temp.left
-            elif value > int(temp.value):
-                if temp.right is not None:
+            else:
+                if temp.right is None:
                     temp.right = new_node
                     return True
                 temp = temp.right
-            elif value == int(temp.value):
-                return False
-
     
 
 if __name__ == '__main__':
     my_tree=BinarySearchTree()
-    print(my_tree.insert(40))
-    
-    print(my_tree.insert(80))
-#    print(my_tree.rootptr.left.value)
+    my_tree.insert(40)
+    my_tree.insert(80)
+    my_tree.insert(30)
+    print (my_tree.root.value)
+    print (my_tree.root.left.value)
+    print (my_tree.root.right.value)
+#    print(my_tree.root.left.value)
 #    my_tree.insert(50)
-#    print(my_tree.rootptr.right.value)
+#    print(my_tree.root.right.value)
