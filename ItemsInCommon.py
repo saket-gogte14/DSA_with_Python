c94e76d5b4abbee5accfd74393dbcda3a931eb71
Saket Gogte <saket.gogte14@gmail.com>
2021-09-10 21:04:45 +0530

    Changes to items to common script

--- ItemsInCommon.py
+++ ItemsInCommon.py
@@ -4,13 +4,13 @@ def items_in_common(list1, list2):
 
     for item in list1:
         if item in list2:
-            common_items.append(item)
+            return True
 
     return common_items
 
 if __name__ == '__main__':
-    list1=[1,4,5,8,9,10,44,77]
-    list2=[6,5,7,6,7,8,9,10,44,5,6,7,898]
+    list1=[1,4,5]
+    list2=[6,5,7]
 
     print(items_in_common(list1,list2))
 
